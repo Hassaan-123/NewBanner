a908ffbcb8ea0b23736ecfc4fadceac19f24529d
cedcoss <cedcoss@cedcosss-Mac-mini.local>
2021-04-20 12:50:51 +0530

    nine commit

--- NewBanner/NewBanner.swift
+++ NewBanner/NewBanner.swift
@@ -48,12 +48,17 @@ public class NewBanner: UIViewController
         maincollection.delegate = self
         maincollection.dataSource = self
         
+       
+    }
+    
+    public func singleimage(myview : UIView)
+    {
         let img = UIImageView()
-        views.addSubview(img)
+        myview.addSubview(img)
         img.translatesAutoresizingMaskIntoConstraints=false
-        img.bottomAnchor.constraint(equalTo: views.bottomAnchor).isActive=true
-        img.leadingAnchor.constraint(equalTo: views.leadingAnchor).isActive=true
-        img.trailingAnchor.constraint(equalTo: views.trailingAnchor).isActive=true
+        img.bottomAnchor.constraint(equalTo: myview.bottomAnchor).isActive=true
+        img.leadingAnchor.constraint(equalTo: myview.leadingAnchor).isActive=true
+        img.trailingAnchor.constraint(equalTo: myview.trailingAnchor).isActive=true
         img.heightAnchor.constraint(equalToConstant: 100).isActive=true
         img.image = UIImage(named: "download1")
     }
@@ -68,7 +73,15 @@ extension NewBanner : UICollectionViewDataSource ,UICollectionViewDelegate
 
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = maincollection.dequeueReusableCell(withReuseIdentifier: "Collections", for: indexPath) as! NewBannerCell
-        cell.image = images[indexPath.row]
+        //cell.image = images[indexPath.row]
+        let img = UIImageView()
+        cell.addSubview(img)
+        img.translatesAutoresizingMaskIntoConstraints=false
+        img.topAnchor.constraint(equalTo: cell.topAnchor).isActive=true
+        img.leadingAnchor.constraint(equalTo: cell.leadingAnchor).isActive=true
+        img.trailingAnchor.constraint(equalTo: cell.trailingAnchor).isActive=true
+        img.bottomAnchor.constraint(equalTo: cell.bottomAnchor).isActive=true
+        img.image = UIImage(named: images[indexPath.row])
         return cell
     }
 }
